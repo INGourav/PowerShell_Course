a9f98ccc50ecc416cfd6f962c44cf899c2c29e1a
GouravIN <gouravin@outlook.com>
2021-10-11 13:08:38 +0530

    adding switch

---
+++ switch.ps1
@@ -0,0 +1,12 @@
+$servers = @("dc01", "dc02", "dc03", "localhost")
+
+foreach ($server in $servers) {
+    $os = Get-WmiObject -Class win32_operatingsystem -ComputerName $server -ErrorAction SilentlyContinue
+    switch ($os.Caption) {
+        "Microsoft Windows 10 Enterprise"
+        { Write-Output "This is Client OS, Windows10" }
+        "Microsoft Server 2012 R2 Standard"
+        { Write-Output "This is Server OS, 2012 R2" }
+        Default { "The OS is neither 2012R2 nor Windows10" }
+    }
+}
